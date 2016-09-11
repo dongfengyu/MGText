2b5b1e8f7496422002df886885f824172fc3d56a
spf <spf@mogoroom.com>
2016-09-11 19:52:23 +0800

    spa

---
+++ MGText/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MGText
+//
+//  Created by song on 16/9/11.
+//  Copyright © 2016年 song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var textString:String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textString = "我是来测试的"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
